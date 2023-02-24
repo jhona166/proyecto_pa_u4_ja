bd3a99c715efb46a336f71e82fac7546f24c6db5
Jhonatan Altamirano <jdaltamiranoo1@uce.edu.ec>
2023-02-24 18:50:49 -0500

    Unidad 4

---
+++ src/main/java/com/example/demo/ProyectoPaU4JaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPaU4JaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPaU4JaApplication.class, args);
+	}
+
+}
